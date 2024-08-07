be66515d441d75c45582985909f1817da84f445d
Carl Brand <11300674+brandc87@users.noreply.github.com>
2024-08-07 23:13:15 +0100

    set page title in ChatRoomLive

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -20,7 +20,7 @@ defmodule SlaxWeb.ChatRoomLive do
           Chat.get_first_room!()
       end
 
-    {:noreply, assign(socket, hide_topic?: false, room: room)}
+    {:noreply, assign(socket, hide_topic?: false, page_title: "#" <> room.name, room: room)}
   end
 
   def render(assigns) do
